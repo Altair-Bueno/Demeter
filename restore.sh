01dbdd721dc08cc3284393d7bff304344d985b44
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2023-01-28 13:48:53 +0100

    chores: Update test.yaml

--- restore.sh
+++ restore.sh
@@ -13,94 +13,78 @@
 # To see the full list of installed utilities, see [README.md](README.md)     #
 ###############################################################################
 
+###############################################################################
+# Settings
+###############################################################################
+set -ex
 # Remote VCS
-export REMOTE_URL="https://github.com/Altair-Bueno/Demeter"
+export REMOTE_URL="${REMOTE_URL:-https://github.com/Altair-Bueno/Demeter}"
 # Where is the backup folder. VSC folder name and system folder must match
 export DEMETER="$HOME/Demeter"
+###############################################################################
 
+function preamble {
+	required_paths=( "$HOME/.cache/zsh/" )
 
-# Create necessary folders
-mkdir -p "$HOME/.cache/zsh/"
-
-# Restoring backup
-if [[ -d "$DEMETER" ]]
-then
-	# "$DEMETER" folder already exist on this machine, testing origin
-	echo "Found Demeter folder on $DEMETER, checking source"
-	cd "$DEMETER" || exit 100
-	if [[ $(git config --get remote.origin.url) == "$REMOTE_URL" ]]
-	then
-		echo "Updating backup..."
-		git pull
-	else 
-		echo "This script expected a different remote server, exiting..."
-		exit 1
-	fi
-else 
-	echo "Found no backup on $DEMETER. Cloning from remote"
-
-	export PARENT_FOLDER=$(dirname "$DEMETER")
-	cd "$PARENT_FOLDER" || exit 100
-
-	git clone "$REMOTE_URL"
-fi
-
-
-# Create links
-cd "$DEMETER/backup" || exit 100
-
-echo "Creating links"
-for TEMP in *(D)
-do
-	echo "Linking $TEMP"
-	# Delete the file before making the symlink, just in case
-	rm "$HOME/$TEMP" 2> /dev/null
-	ln -nfs "$DEMETER/backup/$TEMP" "$HOME/$TEMP"
-done
-
-function install_software_macos() {
-  echo Install xcode commandline tools
-
-  xcode-select --install
-
-  # From https://brew.sh . https://github.com/Homebrew/install/#install-homebrew-on-macos-or-linux
-  echo Installing HomeBrew
-  export NONINTERACTIVE=1
-  command -v brew > /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
-
-  echo Installing software
-  brew install $(cat "$DEMETER/brew_packages.txt" | xargs)
-
-  # From https://ohmyz.sh/#install
-  echo Installing omz
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-  echo Installing omz plugins
-  git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-  git clone https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+	for p in $required_paths
+	do
+		mkdir -p "$p"
+	done
 }
 
-# Creating vim folders
-cd "$HOME" || exit 100
-mkdir -p "$HOME/.vim/undodir" 2> /dev/null
+function clone_demeter_repository {
+	# Restoring backup
+	if [[ -d "$DEMETER" ]]
+	then
+		: "Found already existing Demeter folder ($DEMETER)"
+		if [[ $(git -C "$DEMETER" config --get remote.origin.url) == "$REMOTE_URL" ]]
+		then
+			echo "Updating backup..."
+			git -C "$DEMETER" pull
+		else 
+			: "This script expected a different remote server, exiting..."
+			exit 1
+		fi
+	else 
+		: "Found no backup on $DEMETER. Cloning from remote"
+		git -C "$(dirname "$DEMETER")" clone "$REMOTE_URL"
+	fi
+}
 
-# From astronvim docs
-git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-# Link user folder
-ln -s "$DEMETER/astrovim" "$~/.config/nvim/lua/user"
+function link_backup {
+	: "Linking backup"
+	for TEMP in $DEMETER/backup/*(D)
+	do
+		# Delete the file before making the symlink, just in case
+		rm "$HOME/$TEMP" 2> /dev/null || true
+		ln -nfs "$TEMP" "$HOME/"
+	done
+}
+
+function setup_macos() {
+  : Install xcode commandline tools
+  xcode-select --install
+
+  : Installing HomeBrew and software "https://github.com/Homebrew/install/#install-homebrew-on-macos-or-linux"
+  export NONINTERACTIVE=1
+  command -v brew > /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+  cat "$DEMETER/brew_packages.txt" | xargs brew install
+
+  : Link macos config
+  rm "$HOME/.gitconfig" 2> /dev/null
+  ln -nfs "$DEMETER/macOS/.gitconfig" "$HOME/.gitconfig"
+
+  : Setting up git credential helper "https://gist.github.com/nepsilon/0fd0c779f76d7172f12477ba9d71bb66"
+  git config --global credential.helper osxkeychain
+}
+
+preamble
+clone_demeter_repository
+link_backup
 
 if [[ $(uname) == 'Darwin' ]]
 then
-  # macOS specific config
-	rm "$HOME/.gitconfig" 2> /dev/null
-	ln -nfs "$DEMETER/macOS/.gitconfig" "$HOME/.gitconfig"
-
-  install_software_macos
-
-  # https://gist.github.com/nepsilon/0fd0c779f76d7172f12477ba9d71bb66
-  echo Setting up git credential helper
-  git config --global credential.helper osxkeychain
-
+	setup_macos
 elif [[ $(uname) == 'Linux' ]]
 then
     # Linux specific config
@@ -113,5 +97,3 @@ then
 	rm "$HOME/.gitconfig"
 	ln -nfs "$DEMETER/bsd/.gitconfig" "$HOME/.gitconfig"
 fi
-
-exit 0
